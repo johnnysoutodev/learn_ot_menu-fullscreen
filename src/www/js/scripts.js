4983b686553a0f9b82578c98c2e3a8f2b1441867
Johnny Souto <johnny.souto@icloud.com>
2019-11-23 23:53:04 -0300

    updates

--- src/www/js/scripts.js
+++ src/www/js/scripts.js
@@ -1,4 +1,7 @@
 function menuToogle(){
   var nav = document.getElementById('menu-overlay');
   nav.classList.toggle('active');
+
+  var icon = document.getElementById('toogleIcon');
+  icon.classList.toggle('active');
 }
